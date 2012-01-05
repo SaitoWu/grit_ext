a945ca549ab4bd51743dcfa0dec792fc4e6014a8
Saito <saitowu@gmail.com>
2012-01-05 12:00:59 +0800

    add text? binary? image? support on blob

--- lib/grit_ext/blob.rb
+++ lib/grit_ext/blob.rb
@@ -1,5 +1,17 @@
 module Grit
   class Blob
+    def text?
+      !!(mime_type =~ /application|text/ && !binary?)
+    end
+
+    def image?
+      !!(mime_type =~ /image/)
+    end
+
+    def binary?
+      data[0..1024].include?("\000")
+    end
+
     def encoding
       if detection = CharlockHolmes::EncodingDetector.detect(data)
         detection[:encoding]
