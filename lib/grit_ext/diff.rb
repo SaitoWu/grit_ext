8d34ac5ce7b0e28d4e480194e1801621012edd29
Sato Hiroyuki <sathiroyuki@gmail.com>
2012-11-24 14:19:12 +0900

    Fix diff encoding for non ascii paths and non utf-8 file encoding
    * The encoding of path is dependent on environment, but that of diff body is dependent on file encoding.

--- lib/grit_ext/diff.rb
+++ lib/grit_ext/diff.rb
@@ -10,7 +10,14 @@ module Grit
     end
 
     def diff
-      GritExt.encode! @diff
+      lines = @diff.lines.to_a
+
+      # path encoding is dependent on environment
+      path = GritExt.encode! lines.shift(2).join
+      # diff body encoding is dependent on file encoding
+      body = GritExt.encode! lines.join
+
+      @diff = path + body
     end
   end
 end
